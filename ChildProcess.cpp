7736f09476fc68f695d05387efb3a949ffd85b28
Rachel Fischmar <fischmrl@miamioh.edu>
2026-05-03 18:43:05 -0500

    fix shell bugs and output

--- ChildProcess.cpp
+++ ChildProcess.cpp
@@ -1,6 +1,3 @@
-#ifndef CHILD_PROCESS_CPP
-#define CHILD_PROCESS_CPP
-
 /**
  * This source file contains the implementation for the methods 
  * in the ChildProcess class.
@@ -16,23 +13,17 @@
 
 void
 ChildProcess::myExec(StrVec argList) {
-    std::vector<char*> args;    // list of pointers to args
+    std::vector<char*> args;
     for (auto& s : argList) {
-        args.push_back(&s[0]);  // address of 1st character
+        args.push_back(&s[0]);
     }
-    // nullptr is very important
     args.push_back(nullptr);
-    // Make execvp system call to run desired process
     execvp(args[0], &args[0]);
-    // In case execvp ever fails, we throw a runtime execption
     throw std::runtime_error("Call to execvp failed!");
 }
 
-// Implement the constructor
 ChildProcess::ChildProcess() : childPid(-1) {}
 
-// Implement the destructor.  The destructor is an empty method
-// because this class does not have any resources to release.
 ChildProcess::~ChildProcess() {}
 
 int ChildProcess::forkNexec(const StrVec& strVec) {
@@ -44,10 +35,7 @@ int ChildProcess::forkNexec(const StrVec& strVec) {
 }
 
 int ChildProcess::wait() const {
-    int exitCode = 0;
-    waitpid(childPid, &exitCode, 0);
-    return exitCode;
+    int status = 0;
+    waitpid(childPid, &status, 0);
+    return WEXITSTATUS(status);
 }
-
-#endif
-
--- customLinuxShell.cpp
+++ customLinuxShell.cpp
@@ -5,45 +5,30 @@
  */
 
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <string>
 #include <iomanip>
-#include <unordered_map>
 #include <vector>
 #include <stdexcept>
-#include <algorithm>
 #include <boost/asio.hpp>
 #include "ChildProcess.h"
 
 using namespace boost::asio;
 using namespace boost::asio::ip;
-using namespace std;
 
-// A convenience shortcut to a vector-of-strings
 using StrVec = std::vector<std::string>;
 
 /**
- * Helper method to setup a TCP stream for downloading data from an
+ * Helper method to setup a TCP stream for downloading data from a
  * web-server.
- * 
- * @param host The host name of the web-server. Host names can be of
- * the from "www.miamioh.edu" or "ceclnx01.cec.miamioh.edu".  This
- * information is typically extracted from a given URL.
  *
- * @param path The path to the file being download.  An example of
- * this value is ""
- *
- * @param socket The TCP stream (aka socket) to be setup by this
- * method.  After a successful call to this method, this stream will
- * contain the response from the web-server to be processed.
- *
- * @param port An optional port number. The default port number is "80"
- *
+ * @param hostName The host name of the web-server.
+ * @param path The path to the file being downloaded.
+ * @param data The TCP stream to be set up by this method.
+ * @param port An optional port number. Defaults to "80".
  */
 void setupDownload(const std::string& hostName, const std::string& path,
                    tcp::iostream& data, const std::string& port = "80") {
-    // Create a boost socket and request the log file from the server.
     data.connect(hostName, port);
     data << "GET "   << path     << " HTTP/1.1\r\n"
          << "Host: " << hostName << "\r\n"
@@ -51,10 +36,9 @@ void setupDownload(const std::string& hostName, const std::string& path,
 }
 
 /**
- * This function runs the user-specified command using fork, exec, and waitpid.
- * 
- * @param words The string vector of words entered by the user.
- * 
+ * Forks and executes the given command, then prints its exit code.
+ *
+ * @param words The command and its arguments.
  */
 void cp(std::vector<std::string>& words) {
     ChildProcess cp;
@@ -64,40 +48,35 @@ void cp(std::vector<std::string>& words) {
         std::cout << " " << i;
     }
     std::cout << std::endl;
-    int i = cp.wait();
-    std::cout << "Exit code: " << i << std::endl;
+    int exitCode = cp.wait();
+    if (exitCode != 0) {
+        std::cout << "Exit code: " << exitCode << std::endl;
+    }
 }
 
 /**
- * This function reads in a URL entered by the user and uses the setupDownload
- * helper method to setup a TCP stream for downloading data from a
- * web-server.
- * 
- * @param str the URL specified by the user.
- * 
- * @param url the TCP stream to be set up.
- * 
+ * Parses a URL string and sets up a TCP stream to the web-server.
+ *
+ * @param str The URL specified by the user.
+ * @param stream The TCP stream to be set up.
  */
-void url(std::string& str, tcp::iostream& url) {
-    std::string url1 = str;
-    url1 = url1.substr(url1.find('/') + 2);
-    int i = url1.find('/');
-    std::string url2 = "/" + url1.substr(i + 1);
-    url1 = url1.substr(0, i);
-    setupDownload(url1, url2, url);
+void url(std::string& str, tcp::iostream& stream) {
+    std::string host = str.substr(str.find('/') + 2);
+    size_t slash = host.find('/');
+    std::string path = "/" + host.substr(slash + 1);
+    host = host.substr(0, slash);
+    setupDownload(host, path, stream);
 }
 
 /**
- * This function runs the serial transmission of the specified processes.
- * 
- * @param str The URL entered by the user.
- * 
+ * Fetches a list of commands from the given URL and runs them serially.
+ *
+ * @param str The URL to fetch commands from.
  */
 void serial(std::string& str) {
     tcp::iostream is;
     url(str, is);
-    std::string line;
-    for (std::string hdr; std::getline(is, hdr) 
+    for (std::string hdr; std::getline(is, hdr)
             && !hdr.empty() && hdr != "\r";) {}
     for (std::string line; std::getline(is, line);) {
         std::istringstream in(line);
@@ -113,91 +92,78 @@ void serial(std::string& str) {
 }
 
 /**
- * This function runs the specified command in the para function.
- * 
- * @param nums A string vector of the numbers from the specified URL.
- * 
- * @param words A string vector of the commands from the specified URL.
+ * Forks a child process for the given command and records it for later waiting.
+ *
+ * @param procs A vector to store the forked ChildProcess objects.
+ * @param words The command and its arguments.
  */
-void paraCP(StrVec& nums, StrVec& words) {
-    nums.push_back(words.at(1));
+void paraCP(std::vector<ChildProcess>& procs, StrVec& words) {
+    std::cout << "Running:";
+    for (auto& w : words) std::cout << " " << w;
+    std::cout << std::endl;
     ChildProcess cp;
-    std::cout << "Running: " 
-        << words.at(0) << " " << words.at(1) << std::endl;
-    cp.forkNexec({words.at(0), words.at(1)});
+    cp.forkNexec(words);
+    procs.push_back(std::move(cp));
 }
 
 /**
- * This function runs the specified processes in parallel.
- * 
- * @param str The URL entered by the user.
- * 
+ * Fetches a list of commands from the given URL and runs them in parallel.
+ *
+ * @param str The URL to fetch commands from.
  */
 void para(std::string& str) {
     tcp::iostream is;
     url(str, is);
-    std::string line;
-    StrVec nums;
-    ChildProcess cp;
+    std::vector<ChildProcess> procs;
     for (std::string line; std::getline(is, line);) {
         std::istringstream in(line);
-        std::string word; 
+        std::string word;
         StrVec words;
         while (in >> std::quoted(word)) words.push_back(word);
         if (words.empty()) continue;
         if (words.at(0) != "#") {
-            if (words.at(0) == "sleep") {
-                paraCP(nums, words);
-            }
+            paraCP(procs, words);
         }
     }
-    for (auto& i : nums) {
-        std::string p = i + " ";
-        int j = cp.wait();
-        std::cout << "Exit code: " << j << std::endl;
+    for (auto& proc : procs) {
+        int exitCode = proc.wait();
+        if (exitCode != 0) {
+            std::cout << "Exit code: " << exitCode << std::endl;
+        }
     }
 }
 
 /**
- * This function takes the user input and determines which command to execute
- * based on the command line arguments entered.
+ * Runs the main shell loop, reading and dispatching commands from the given
+ * input stream.
  *
- * @param is The user input in the terminal.
- * 
- * @param prompt The symbol to prompt the user to enter a line in the terminal.
- * 
+ * @param is The input stream to read commands from. Defaults to std::cin.
+ * @param prompt The prompt string displayed to the user. Defaults to "> ".
  */
 void process(std::istream& is = std::cin, const std::string& prompt = "> ") {
-        std::string line;
-        while (std::cout << prompt, std::getline(std::cin, line)) {
-            std::istringstream is(line);
-            std::string word, tmp;
-            std::vector<std::string> words;
-            while (is >> std::quoted(word)) words.push_back(word);
-            if (words.empty()) continue;
-            tmp = words.at(0);
-            if (tmp != "#" && !tmp.empty()) {
-                if (tmp == "exit") {
-                    break;
-                } else if (tmp == "SERIAL") {
-                    serial(words.at(1));
-                } else if (tmp == "PARALLEL") {
-                    para(words.at(1));
-                } else {
-                    cp(words);
-                }
+    std::string line;
+    while (std::cout << prompt, std::getline(is, line)) {
+        std::istringstream ss(line);
+        std::string word, tmp;
+        std::vector<std::string> words;
+        while (ss >> std::quoted(word)) words.push_back(word);
+        if (words.empty()) continue;
+        tmp = words.at(0);
+        if (tmp != "#" && !tmp.empty()) {
+            if (tmp == "exit") {
+                break;
+            } else if (tmp == "SERIAL") {
+                serial(words.at(1));
+            } else if (tmp == "PARALLEL") {
+                para(words.at(1));
+            } else {
+                cp(words);
             }
         }
+    }
 }
 
-/**
- * The main function that calls the process function.
- *
- * \param[in] argc The number of command-line arguments.  This program
- * requires exactly one command-line argument.
- *
- * \param[in] argv The actual command-line argument. This should be an URL.
- */
-int main(int argc, char *argv[]) {
+int main() {
     process();
+    return 0;
 }
